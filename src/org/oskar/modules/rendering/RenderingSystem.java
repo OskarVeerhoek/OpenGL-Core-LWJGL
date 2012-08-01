6b633787e9d5d6417610b6f7fcb568baa75cc8c1
Oskar Veerhoek <thecodinguniverse@gmail.com>
2012-08-01 13:32:47 +0200

    documented the createBuffers method in RenderingSystem

--- src/org/oskar/modules/rendering/RenderingSystem.java
+++ src/org/oskar/modules/rendering/RenderingSystem.java
@@ -98,43 +98,97 @@ public class RenderingSystem implements GameModule {
 
     private void createBuffers() {
         gameWorld.debug(RenderingSystem.class, "Creating VAO");
+        // >> Vertex Array Objects (VAO) are OpenGL Objects that store the
+        // >> set of bindings between vertex attributes and the user's source
+        // >> vertex data. (http://www.opengl.org/wiki/Vertex_Array_Object)
+        // >> glGenVertexArrays returns n vertex array object names in arrays.
+        // Create one VAO and store it in int vao.
         vao = glGenVertexArrays();
+        // >> glBindVertexArray binds the vertex array object with name array.
+        // Bind the aforementioned VAO to OpenGL.
         glBindVertexArray(vao);
         gameWorld.debug(RenderingSystem.class, "Creating IBO");
+        // >> glGenBuffers returns n buffer object names in buffers.
+        // >> No buffer objects are associated with the returned buffer object names
+        // >> until they are first bound by calling glBindBuffer.
+        // Create an OpenGL buffer that we'll use as an index buffer object (stores
+        // indices that point to the data in the vertex buffer object).
         ibo = glGenBuffers();
+        gameWorld.debug(RenderingSystem.class, "Creating VBO");
+        // Create an OpenGL buffer that we'll use as a vertex buffer object (stores
+        // vertex position and colour data).
+        vbo = glGenBuffers();
+        // >> glBindBuffer binds a buffer object to the specified buffer binding point.
+        // Bind ibo to GL_ELEMENT_ARRAY_BUFFER.
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-        IntBuffer indexData = asIntBuffer(new int[]{
+        // Store the index data inside an IntBuffer and make it readable to OpenGL.
+        IntBuffer indexData = asFlippedIntBuffer(new int[]{
                 0, 1, 2,
                 0, 2, 3
         });
-        indexData.flip();
+        // >> glBufferData creates a new data store for the buffer object currently bound
+        // >> to target. Any pre-existing data store is deleted. The new data store is created
+        // >> with the specified size in bytes and usage. If data is not NULL, the data
+        // >> store is initialized with data from this pointer. In its initial state, the
+        // >> new data store is not mapped, it has a NULL mapped pointer, and its mapped
+        // >> access is GL_READ_WRITE.
+        // Store the index data inside the IBO.
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexData, GL_STATIC_DRAW);
+        // Unbind the IBO.
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-        gameWorld.debug(RenderingSystem.class, "Creating VBO");
-        vbo = glGenBuffers();
+        // Bind vbo to GL_ARRAY_BUFFER.
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        // Store the vertex position data inside a FloatBuffer and make it readable to OpenGL.
         FloatBuffer vertexPositionData = asFlippedFloatBuffer(new float[] {
                 -1.0f, -1.0f,
                 +1.0f, -1.0f,
                 +1.0f, +1.0f,
                 -1.0f, +1.0f,
         });
+        // Calculate the size of the FloatBuffer in bytes.
         int vertexPositionDataSize = /* amount of elements */ 4 * /* amount of components */ 2 * /* size of float */ 4;
+        // Store the vertex colour data inside a FloatBuffer and make it readable to OpenGL.
         FloatBuffer vertexColourData = asFlippedFloatBuffer(new float[] {
                 +1.0f, +0.0f, +0.0f,
                 +0.0f, +1.0f, +0.0f,
                 +0.0f, +0.0f, +1.0f,
                 +1.0f, +1.0f, +1.0f
         });
+        // Calculate the size of the FloatBuffer in bytes.
         int vertexColourDataSize = /* amount of elements */ 4 * /* amount of components */ 3 * /* size of float */ 4;
+        // Allocate enough size in vbo to support the vertexPositionData and colourPositionData FloatBuffers.
         glBufferData(GL_ARRAY_BUFFER, vertexPositionDataSize + vertexColourDataSize, GL_STATIC_DRAW);
+        // >> glBufferSubData redefines some or all of the data store for the buffer object currently bound to target.
+        // >> int target = type of buffer object (VBO = GL_ARRAY_BUFFER, IBO = GL_ELEMENT_ARRAY_BUFFER)
+        // >> int offset = offset in bytes in the buffer object's data store where data will be stored
+        // >> FloatBuffer data = new data that will be copied into the data store
+        // Store the vertex position data at offset 0.
         glBufferSubData(GL_ARRAY_BUFFER, 0, vertexPositionData);
+        // Store the vertex colour data after the vertex position data.
         glBufferSubData(GL_ARRAY_BUFFER, vertexPositionDataSize, vertexColourData);
+        // >> glEnableVertexAttribArray enables the generic vertex attribute array specified by index.
+        // >> glDisableVertexAttribArray disables the generic vertex attribute array specified by
+        // >> index. By default, all client-side capabilities are disabled, including all generic
+        // >> vertex attribute arrays. If enabled, the values in the generic vertex attribute array
+        // >> will be accessed and used for rendering when calls are made to vertex array commands
+        // >> such as glDrawArrays, glDrawElements, glDrawRangeElements, glMultiDrawElements, or glMultiDrawArrays.
+        // Enable the vertex position attribute.
         glEnableVertexAttribArray(VERTEX_POSITION);
+        // Enable the vertex colour attribute.
         glEnableVertexAttribArray(VERTEX_COLOUR);
+        // >> glVertexAttribPointer and glVertexAttribIPointer specify the location and data format of the
+        // >> array of generic vertex attributes at index index to use when rendering. size specifies
+        // >> the number of components per attribute and must be 1, 2, 3, 4, or GL_BGRA. type specifies
+        // >> the data type of each component, and stride specifies the byte stride from one attribute
+        // >> to the next, allowing vertices and attributes to be packed into a single array or stored
+        // >> in separate arrays.
+        // Tell OpenGL where to find the vertex position data inside the VBO.
         glVertexAttribPointer(VERTEX_POSITION, 2, GL_FLOAT, false, 0, 0);
+        // Tell OpenGL where to find the vertex colour data inside the VBO.
         glVertexAttribPointer(VERTEX_COLOUR, 3, GL_FLOAT, false, 0, 32);
+        // Unbind the vertex buffer object.
         glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // Unbind the vertex array object.
         glBindVertexArray(0);
         checkForErrors();
     }
